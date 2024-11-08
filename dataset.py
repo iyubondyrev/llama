e36efc21ad834425fafae22432a097fbf566d0a0
Ivan Bondyrev <ivan.bondivan-bond@outlook.com>
2024-11-08 09:05:44 +0100

    change dataset impl

--- dataset.py
+++ dataset.py
@@ -1,33 +1,27 @@
-import subprocess
-import torch
 from torch.utils.data import Dataset
-import gzip
-from tqdm import tqdm
+import os
+import torch
+import numpy as np
 
-class TokenizedChunksDataset(Dataset):
-    def __init__(self, file_path, seq_len=1024, lines_to_read: int | None = None):
-        self.file_path = file_path
+class BinaryTokenDataset(Dataset):
+    def __init__(self, bin_file, seq_len=1024):
+        self.bin_file = bin_file
         self.seq_len = seq_len
-        self.data = []
-        total_lines = 1209636
-
-        total_lines = total_lines if lines_to_read is None else min(total_lines, lines_to_read)
-        print("Total lines: ", total_lines)
-
-        with gzip.open(self.file_path, 'rt') as f:
-            i = 0
-            for line in tqdm(f, total=total_lines, desc="Reading file and loading it to the memory"):
-                if lines_to_read is not None and i > lines_to_read:
-                    break
-                tokens = list(map(int, line.strip().split()))
-                assert len(tokens) == self.seq_len, "Each sequence must have 1024 tokens."
-                self.data.append(tokens)
-                i += 1
+        self.record_size = seq_len * 4
+        
+        self.total_records = os.path.getsize(bin_file) // self.record_size
+        print(f"Total sequences available: {self.total_records}")
 
     def __len__(self):
-        return len(self.data)
+        return self.total_records
 
     def __getitem__(self, idx):
-        input_ids = torch.tensor(self.data[idx], dtype=torch.long)
+        if idx >= self.total_records:
+            raise IndexError("Index out of range")
 
-        return {'input_ids': input_ids[:-1], 'labels': input_ids[1:]}
+        with open(self.bin_file, 'rb') as f:
+            f.seek(idx * self.record_size)
+            data = np.frombuffer(f.read(self.record_size), dtype=np.int32)
+            
+            input_ids = torch.tensor(data, dtype=torch.long)
+            return {'input_ids': input_ids[:-1], 'labels': input_ids[1:]}
